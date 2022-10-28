cf0a30c59d4733fa755a49826994648371988418
FabianaMendes <fabianamendesmg@yahoo.com.br>
2022-10-27 21:23:23 -0300

    Cria projeto, monta arquivos e estilos, cria script

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("form");
+const user = document.getElementById("user");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = {
+        user: user.value,
+        password: password.value
+    }
+    
+    localStorage.setItem('user', data);
+
+    fetch("welcome.html", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        })
+        .finally(() => {
+            window.open('welcome.html', "_self");
+        })
+})
